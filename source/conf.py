b709a283996ecd5c935b21bc409920e9b17db572
Chandola, Varun <chandola@buffalo.edu>
2020-01-27 12:38:13 -0500

    modifying conf

--- source/conf.py
+++ source/conf.py
@@ -21,6 +21,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import ablog
 
 # -- General configuration ------------------------------------------------
 
@@ -36,7 +37,6 @@ extensions = ['sphinx.ext.mathjax',
     'ablog']
 
 # 2. Add ablog templates path
-import ablog
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
